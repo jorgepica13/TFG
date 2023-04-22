6587b64b27b8ecde8fc9892801b62fe2d91cdd78
jorgepica13 <jorgepica13@correo.ugr.es>
2023-04-22 12:52:37 +0200

    añadidios codigos auxiliares para la segmentacion

---
+++ seg_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: JORGE PICADO CARINO
+"""
+
+ALL_CLASSES = ['background', 'elliptical', 'spiral', 'uncertain']
+
+LABEL_COLORS_LIST = [
+    (0, 0, 0), # Background
+    (1, 1, 1), # Elliptical
+    (2, 2, 2), # Spiral
+    (3, 3, 3), # Uncertain
+]
+
+VIS_LABEL_MAP = [
+    (0, 0, 0), # Background
+    (255, 0, 0), # Elliptical
+    (0, 255, 0), # Spiral
+    (0, 0, 255), # Uncertain
+]
---
+++ seg_metrics.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: JORGE PICADO CARINO
+"""
+
+import torch
+
+def pix_acc(target, outputs, num_classes):
+    """
+    Calculates pixel accuracy, given target and output tensors 
+    and number of classes.
+    """
+    labeled = (target > 0) * (target <= num_classes)
+    _, preds = torch.max(outputs.data, 1)
+    correct = ((preds == target) * labeled).sum().item()
+    return labeled, correct
