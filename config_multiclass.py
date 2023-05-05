3e2a942ac72c797086ac2ab0018f4aeb0e78e840
jorgepica13 <jorgepica13@correo.ugr.es>
2023-05-05 16:33:19 +0200

    archivo para indicar los colores de las mascaras y el color deseado en la salida a la hora de testear

---
+++ config_multiclass.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: JORGE PICADO CARINO
+"""
+
+ALL_CLASSES = ['background', 'elliptical', 'spiral', 'uncertain']
+
+LABEL_COLORS_LIST = [
+    (0, 0, 0), # Background
+    (100, 100, 100), # Elliptical
+    (128, 128, 128), # Spiral
+    (194, 194, 194), # Uncertain
+]
+
+VIS_LABEL_MAP = [
+    (0, 0, 0), # Background
+    (255, 0, 0), # Elliptical
+    (255, 143, 0), # Spiral
+    (255, 255, 0), # Uncertain
+]
