4a0c44d160cffb338a8d81c6cf4fdd5fd3261bb2
jorgepica13 <jorgepica13@correo.ugr.es>
2023-05-22 19:27:01 +0200

    eliminado paso de paginas y añadido barra de progreso

--- Desktop App New/desktopApp.py
+++ Desktop App New/desktopApp.py
@@ -9,60 +9,45 @@
 
 from PIL import ImageTk, Image
 from tkinter import filedialog
-
 import segmentation, cut, restore, classification
-
-#Python program for creating an application to switch pages using trinket.  
-  
 import tkinter as tk  
+from tkinter import ttk
+import time
   
 LARGE_FONT= ("Verdana", 12)  
-
   
 class SeaofBTCapp(tk.Tk):  
   
     def __init__(self, *args, **kwargs):  
-          
         tk.Tk.__init__(self, *args, **kwargs)
         
         self.state('zoomed')
         
         container = tk.Frame(self)  
-  
         container.pack(side="top", fill="both", expand = True)  
-  
         container.grid_rowconfigure(0, weight=1)  
         container.grid_columnconfigure(0, weight=1)  
   
         self.frames = {}  
-  
         for F in (StartPage, PageOne, PageTwo):  
-  
             frame = F(container, self)  
-  
             self.frames[F] = frame  
-  
             frame.grid(row=0, column=0, sticky="nsew")  
   
         self.show_frame(StartPage)  
   
     def show_frame(self, cont):  
-  
         frame = self.frames[cont]  
         frame.tkraise()  
 
+
 def clear_frame(self):
     nwidget = 0
-    
     for widgets in self.winfo_children():
-        #print(widgets)
         if(nwidget > 2):
             widgets.destroy()
         
         nwidget += 1
-    
-    #controller.show_frame(StartPage)
-    
 
 
 # Funcion para cargar un archivo
@@ -71,7 +56,6 @@ def load_file(self):
     
     num_widgets = 0
     for widgets in self.winfo_children():
-        # print(widgets)
         if(num_widgets > 3):
             widgets.destroy()
         
@@ -79,11 +63,10 @@ def load_file(self):
         
     global image_selected
     self.filename = filedialog.askopenfilename(initialdir=r"C:\Users\jorge\Desktop\galaxy_segmentation\test_images",
-                                                 title="Select A File",
-                                                 filetypes=(("jpg files", "*.jpg"), ("all files", "*.*")))
-    #image_created = classify_image(self.filename)
-    
-    label = tk.Label(self, text='La imagen seleccionada es la siguiente: \n')
+                                               title="Select A File",
+                                               filetypes=(("jpg files", "*.jpg"), ("all files", "*.*")))
+        
+    label = tk.Label(self, text='\nLa imagen seleccionada es la siguiente: \n')
     label.pack()
     label.config(font=("Verdana", 16))
     
@@ -91,12 +74,70 @@ def load_file(self):
     label2.pack()
     
     image_selected = ImageTk.PhotoImage(Image.open(self.filename))
-    
     image_selected_label = tk.Label(self, image=image_selected)
     image_selected_label.pack()
     
     global name
     name = self.filename
+    
+
+def show_images(self, path_image):
+    progressbar = ttk.Progressbar()
+    progressbar.place(x=580, y=580, width=200)
+    self.update()
+    
+    global im0, im1, im2, im3
+    
+    seg_im, mask_im = segmentation.seg_inference(path_image)
+    im0 = ImageTk.PhotoImage(Image.open(seg_im))
+    progressbar.step(25)
+    self.update()
+
+    cut_im = cut.cut_inference(path_image, mask_im)
+    im1 = ImageTk.PhotoImage(Image.open(cut_im))
+    progressbar.step(25)
+    self.update()
+    
+    res_im = restore.process_input(cut_im)
+    im2 = ImageTk.PhotoImage(Image.open(res_im))
+    progressbar.step(25)
+    self.update()
+    
+    label_class, confidence_class = classification.class_inference(res_im)
+    im3 = ImageTk.PhotoImage(Image.open(path_image))
+    progressbar.step(24)
+    self.update()
+    
+    time.sleep(2)
+    
+    progressbar.step(1)
+    progressbar.place_forget()    
+    clear_frame(self)
+    self.update()
+    
+    im0_label = tk.Label(self, image=im0, height=256, width=256)
+    im0_label.place(x=50, y=250)
+  
+    im1_label = tk.Label(self, image=im1, height=256, width=256)
+    im1_label.place(x=350, y=250)
+    
+    im2_label = tk.Label(self, image=im2, height=256, width=256)
+    im2_label.place(x=650, y=250)
+
+    im3_label = tk.Label(self, image=im3, height=256, width=256)
+    im3_label.place(x=950, y=250)
+    
+    pred_text = 'Pred: ' + label_class
+    text0 = tk.Label(self, text=pred_text)
+    text0.place(x=950, y=200)
+    
+    conf_text = '% de confianza: ' + str(confidence_class)
+    text1 = tk.Label(self, text=conf_text)
+    text1.place(x=950, y=220)
+    
+    text2 = tk.Label(self, text='Proceso finalizado')
+    text2.place(x=600, y=600)
+    text2.config(font=("Verdana", 14))
 
 
 class StartPage(tk.Frame):  
@@ -106,28 +147,22 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="GALAXIES", font=LARGE_FONT)  
         label.pack(pady=10,padx=10)  
 
+        button = tk.Button(self, text="Load File", height=3, width=20, command=lambda: load_file(self))  
+        button.pack(side='top', pady=15)
 
-        button = tk.Button(self, text="Load File",
-                           height=5,
-                           width=30,
-                           command=lambda: load_file(self))  
-        button.pack(side='top', pady=50)
+#         altura = tk.Frame.winfo_screenheight(self)
+#         anchura = tk.Frame.winfo_screenwidth(self)
+        
+#         paltura = 1 - (150/altura)
+#         panchura = 1 - (150/anchura)
+        
+#         tamx = anchura * panchura
+#         tamy = altura * paltura
+        
+        button2 = tk.Button(self, text="Start Process", height=3, width=20, command=lambda: show_images(self, name))
+        button2.place(x=1120, y=600)
+        
 
-        altura = tk.Frame.winfo_screenheight(self)
-        anchura = tk.Frame.winfo_screenwidth(self)
-        
-        paltura = 1 - (150/altura)
-        panchura = 1 - (150/anchura)
-        
-        tamx = anchura * panchura
-        tamy = altura * paltura
-        
-        button2 = tk.Button(self, text="Process",
-                           height=3,
-                           width=15,
-                           command=lambda: controller.show_frame(PageTwo))
-        button2.place(x=tamx, y=tamy)  
-  
 class PageOne(tk.Frame):
                   
     def __init__(self, parent, controller):
@@ -136,8 +171,7 @@ class PageOne(tk.Frame):
         #label = tk.Label(self, text="CLASSIFICATION", font=LARGE_FONT)  
         #label.pack(pady=10,padx=10)  
   
-        button1 = tk.Button(self, text="Back to Home",  
-                            command=lambda: clear_frame(self, controller))  
+        button1 = tk.Button(self, text="Back to Home", command=lambda: clear_frame(self, controller))  
         button1.place(x=20, y=20)
   
         #label = tk.Label(self, text="Pulse en el boton para cargar \nla imagen que desea clasificar\n")
@@ -152,67 +186,6 @@ class PageOne(tk.Frame):
         # button3.place(x=500, y=10)
   
     
-def show_images(self, path_image):
-    print('INICIO')
-    
-    seg_im, mask_im = segmentation.seg_inference(path_image)
-    
-    global im0
-    im0 = ImageTk.PhotoImage(Image.open(seg_im))
-    
-    im0_label = tk.Label(self, image=im0,
-                         height=256,
-                         width=256)
-    im0_label.place(x=50, y=250)
-    
-    print('SEGMENTATION')
-    
-    cut_im = cut.cut_inference(path_image, mask_im)
-    
-    global im1
-    im1 = ImageTk.PhotoImage(Image.open(cut_im))
-    
-    im1_label = tk.Label(self, image=im1,
-                         height=256,
-                         width=256)
-    im1_label.place(x=350, y=250)
-    
-    print('RESTORATION')
-    
-    res_im = restore.process_input(cut_im)
-    
-    global im2
-    im2 = ImageTk.PhotoImage(Image.open(res_im))
-    
-    im2_label = tk.Label(self, image=im2,
-                         height=256,
-                         width=256)
-    im2_label.place(x=650, y=250)
-    
-    print('CLASSIFICATION')
-    
-    print(res_im)
-    label_class, confidence_class = classification.class_inference(res_im)
-    
-    global im3
-    im3 = ImageTk.PhotoImage(Image.open(path_image))
-    
-    im3_label = tk.Label(self, image=im3,
-                         height=256,
-                         width=256)
-    im3_label.place(x=950, y=250)
-    
-    print(label_class, ' ', confidence_class)
-    pred_text = 'Pred: ' + label_class
-    text0 = tk.Label(self, text=pred_text)
-    text0.place(x=950, y=200)
-    
-    conf_text = '% de confianza: ' + str(confidence_class)
-    text1 = tk.Label(self, text=conf_text)
-    text1.place(x=950, y=220)
-    
-    print('DONE')
-    
 class PageTwo(tk.Frame):  
   
     def __init__(self, parent, controller):  
@@ -220,15 +193,13 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="PROCESS", font=LARGE_FONT)  
         label.pack(pady=10,padx=10)  
   
-        button1 = tk.Button(self, text="Back to Home",  
-                            command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
         button1.place(x=20, y=10)
         
         label = tk.Label(self, text="Pulse en el boton para llevar a cabo el proceso de segmentación y clasificación\n")
         label.pack()
   
-        button2 = tk.Button(self, text="Start Process",  
-                            command=lambda: show_images(self, name))  
+        button2 = tk.Button(self, text="Start Process", command=lambda: show_images(self, name))  
         button2.pack()  
           
 
@@ -236,7 +207,5 @@ name = ''
 app = SeaofBTCapp()
 app.title("App Desktop")  
 app.geometry("600x400")
-
-print(name)
 
 app.mainloop()
